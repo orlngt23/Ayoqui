ba80be723e57ca5806986dd5aba051c0e2a15b5b
orlngt23 <00029221@uca.edu.sv>
2022-11-25 08:48:53 -0600

    Avance Ayoqui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import {Switch, Route} from 'react-router-dom';
+import Products from './components/Products';
+import Product from './components/Product'
+import Cart from "./components/Cart";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Navbar/>
+  <Switch>
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/Products" component={Products}/>
+    <Route exact path="/Products/:id" component={Product}/>
+    <Route exact path="/cart" component={Cart} />
+    <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+
+  </Switch>
+  
+  </>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Products from "./Products";
+
+export default function home() {
+  return (
+    <div classNameName="hero">
+      <div className="card text-bg-dark border-0">
+        <img
+          src="/assets/BaPixelArt.png"
+          className="card-img"
+          alt="Logo-pixel-art"
+        />
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+          <div classNameName="container">
+            <h5 className="card-title display-5 fw-semibold mb-0">
+              Emprendimiento de la Semana
+            </h5>
+            <br />
+            <p className="card-text ">
+              <h5 className="card-text-emprendimiento display-6 fw-semibold mb-0">
+                Pixel Art
+              </h5>
+              <p className="card-text lead fs-2">
+                <br />
+
+                This is a wider card with supporting 
+                <br/>
+                text below as a natural
+                <br />
+                lead-in to additional content.
+              </p>
+            </p>
+          </div>
+        </div>
+      </div>
+      <Products/>
+    </div>
+  );
+}
